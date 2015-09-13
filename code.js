7e440e06e67179a808db898cf8028d57a7bae892
Simon White <simon@trigowhite.com>
2015-09-13 23:16:55 +0100

    try mozilla code for cookie

--- code.js
+++ code.js
@@ -1,15 +1,48 @@
 // Code for the quiz
 
+function createCookie(name, value, days) {
+    var expires;
+
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = ", expires=" + date.toGMTString();
+    } else {
+        expires = "";
+    }
+    var x = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + ", path=/";
+    document.cookie = x;
+}
+
+function readCookie(name) {
+    var nameEQ = encodeURIComponent(name) + "=";
+    var ca = document.cookie.split(',');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+
+
 $(function() {
 
-    var a = Cookies.get('simon');
+    var a = readCookie('simon');
 
     if (a)
         alert('yes');
 
-    Cookies.set('simon', 'something');
+    var cookies = document.cookie;
 
-    var b = Cookies.get('simon');
+    createCookie('simon', 'something', 10);
+
+    var b = readCookie('simon');
 
     if (b)
         alert('yes');
