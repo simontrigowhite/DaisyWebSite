b62f4dcf3bbc089fde6fac87e86ca6e1869cafc6
Simon White <simon@trigowhite.com>
2015-09-13 23:23:50 +0100

    use local storage instead for now

--- code.js
+++ code.js
@@ -1,32 +1,15 @@
 // Code for the quiz
 
 function createCookie(name, value, days) {
-    var expires;
-
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = ", expires=" + date.toGMTString();
-    } else {
-        expires = "";
-    }
-    var x = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + ", path=/";
-    document.cookie = x;
+    localStorage.setItem(name, value);
 }
 
 function readCookie(name) {
-    var nameEQ = encodeURIComponent(name) + "=";
-    var ca = document.cookie.split(',');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
-    }
-    return null;
+    return localStorage.getItem(name);
 }
 
 function eraseCookie(name) {
-    createCookie(name, "", -1);
+    localStorage.removeItem(name);
 }
 
 
@@ -36,7 +19,7 @@ $(function() {
     var a = readCookie('simon');
 
     if (a)
-        alert('yes');
+        alert('yes a');
 
     var cookies = document.cookie;
 
@@ -45,7 +28,9 @@ $(function() {
     var b = readCookie('simon');
 
     if (b)
-        alert('yes');
+        alert('yes b');
+
+    eraseCookie('simon');
 
     $("#nextBit").hide();
     $("#lastBit").hide();
