3b881fae857c6e86377fad031074e400ea8d40a2
Simon White <simon@trigowhite.com>
2015-10-01 15:46:45 +0100

    browse and drop in chrome, browse in IE

--- code.js
+++ code.js
@@ -1,12 +1,38 @@
 // Code for the quiz - the functionality. Written in JavaScript language.
 
 
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+        output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+            f.size, ' bytes, last modified: ',
+            f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+            '</li>');
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+}
+
+
+
 $(document).ready(runPage);
 
 
 function runPage() {
 
-    setupQuestion( {
+    // Check for the various File API support.
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        // Great success! All the File APIs are supported.
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
+
+    document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+
+    setupQuestion({
         trueButton: $("#trueButton"),
         falseButton: $("#falseButton"),
         submitButton: $("#submitButton"),
